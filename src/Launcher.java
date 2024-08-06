bc06587bdb208c69cca2b48f53fe53f4606872b6
Phạm Hưng Thịnh <hungthinh16072005@gmail.com>
2024-08-06 15:27:45 +0700

    chore: Refactor Launcher class to prompt for password length

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome to the Password Generator!");
-        System.out.println("Enter the length of password:");
+        System.out.println("Enter the length of password (at least 8 characters): ");
         Scanner sc = new Scanner(System.in);
         // Error handling:
-        try {
-            int length = sc.nextInt();
-        } catch (Exception e) {
-            System.err.println("Please enter a number!");
-        }
+        
     }
 }
