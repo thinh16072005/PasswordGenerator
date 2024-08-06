92154dadf2a2553842d1305210983f42ebcc7b7a
Phạm Hưng Thịnh <hungthinh16072005@gmail.com>
2024-08-06 09:10:17 +0700

    feat: add PasswordGenerator class for generating random passwords

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class PasswordGenerator {
+    // Character pools
+    public static final String UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBERS = "0123456789";
+    public static final String SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>/?";
+
+    // Create Random object:
+    private final Random rand;
+
+    // Constructor:
+    public PasswordGenerator() {
+        rand = new Random();
+    }
+
+    // Method to randomly generate password with criteria:
+
+    public String generatePassword(
+        int length, 
+        boolean includeUpperCase, 
+        boolean includeLowerCase, 
+        boolean includeNumbers, 
+        boolean includeSymbols) {
+
+        StringBuilder passwordBuilder = new StringBuilder();
+
+        String validCharacters = "";
+
+        if (includeUpperCase) validCharacters += UPPER_CASE;
+        if (includeLowerCase) validCharacters += LOWER_CASE;
+        if (includeNumbers) validCharacters += NUMBERS;
+        if (includeSymbols) validCharacters += SYMBOLS;
+
+        // Generate a password:
+        for (int i = 0; i < length; i++) {
+            // Generate password index:
+            int passwordIndex = rand.nextInt(validCharacters.length());
+
+            // Get the characters based on the index:
+            char getCharacterIndex = validCharacters.charAt(passwordIndex);
+
+            // Store characters in passwordBuilder:
+            passwordBuilder.append(getCharacterIndex);
+        }
+
+        return passwordBuilder.toString();
+    }
+}
