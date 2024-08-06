0dafd6e708e04a48bae0e163367a5733b92c2795
Phạm Hưng Thịnh <hungthinh16072005@gmail.com>
2024-08-06 17:47:22 +0700

    made changes to launcher class

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,16 +1,34 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Launcher {
-
     public static void main(String[] args) {
         System.out.println("Welcome to the Password Generator!");
-        System.out.println("Enter the length of password:");
+        System.out.print("Enter the length of password (at least 8 characters): ");
         Scanner sc = new Scanner(System.in);
-        // Error handling:
+        int length = sc.nextInt();
+        
+        // Initialize objects
+        Criteria cr = new Criteria();
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+        // Output and Error handling:
         try {
-            int length = sc.nextInt();
-        } catch (Exception e) {
-            System.err.println("Please enter a number!");
-        }
+            System.out.println("Choose the following criteria for your password (1 for YES, 0 for NO):");
+            System.out.println("Does the password contain uppercase letters? "); 
+            System.out.println("Does the password contain lowercase letters? ");
+            System.out.println("Does the password contain numbers? ");
+            System.out.println("Does the password contain special symbols? ");
+            System.out.println(passwordGenerator.generatePassword(
+                length,
+                cr.isIncludeUpperCase(),
+                cr.isIncludeLowerCase(),
+                cr.isIncludeNumbers(), 
+                cr.isIncludeSymbols()
+        ));
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+        } finally {System.out.println("The password has been generated!");}
+        sc.close();   
     }
 }
