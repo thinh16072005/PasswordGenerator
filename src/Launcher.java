def935d60ae22f10e6bdefa28d2f67842f828531
Phạm Hưng Thịnh <hungthinh16072005@gmail.com>
2024-08-06 09:26:06 +0700

    chore: Add Launcher and Menu classes for user interaction

---
+++ src/Launcher.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Password Generator!");
+        System.out.println("Enter the length of password:");
+        Scanner sc = new Scanner(System.in);
+        // Error handling:
+        try {
+            int length = sc.nextInt();
+        } catch (Exception e) {
+            System.err.println("Please enter a number!");
+        }
+    }
+}
